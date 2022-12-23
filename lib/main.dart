e521d129b503d8c42819a508016dd4d55b448b83
dnadanushka <dnadanushka@gmail.com>
2022-12-24 02:04:29 +0530

    working for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_starter/core/config/router.router.dart';
-import 'package:flutter_starter/locator.dart';
-import 'package:flutter_starter/ui/theme/theme.dart';
-import 'package:stacked_services/stacked_services.dart';
+import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  setupLocator();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final SmsQuery _query = SmsQuery();
+  List<SmsMessage> _messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Starter',
-      navigatorKey: StackedService.navigatorKey,
-      onGenerateRoute: StackedRouter().onGenerateRoute,
-      theme: AppTheme.theme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: _messages.length,
+            itemBuilder: (BuildContext context, int i) {
+              var message = _messages[i];
+
+              return ListTile(
+                title: Text('${message.sender} [${message.date}]'),
+                subtitle: Text('${message.body}'),
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            var permission = await Permission.sms.status;
+            if (permission.isGranted) {
+              final messages = await _query.querySms(
+                kinds: [SmsQueryKind.inbox, SmsQueryKind.sent],
+                // address: '+254712345789',
+                count: 10,
+              );
+              debugPrint('sms inbox messages: ${messages.length}');
+
+              setState(() => _messages = messages);
+            } else {
+              await Permission.sms.request();
+            }
+          },
+          child: const Icon(Icons.refresh),
+        ),
+      ),
     );
   }
 }
